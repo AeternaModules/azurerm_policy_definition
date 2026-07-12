7f921f4e57a9e545044df88ad883be1e97fd8764
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:19:39 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,45 +1,41 @@
-output "policy_definitions" {
-  description = "All policy_definition resources"
-  value       = azurerm_policy_definition.policy_definitions
-}
 output "policy_definitions_description" {
-  description = "List of description values across all policy_definitions"
-  value       = [for k, v in azurerm_policy_definition.policy_definitions : v.description]
+  description = "Map of description values across all policy_definitions, keyed the same as var.policy_definitions"
+  value       = { for k, v in azurerm_policy_definition.policy_definitions : k => v.description }
 }
 output "policy_definitions_display_name" {
-  description = "List of display_name values across all policy_definitions"
-  value       = [for k, v in azurerm_policy_definition.policy_definitions : v.display_name]
+  description = "Map of display_name values across all policy_definitions, keyed the same as var.policy_definitions"
+  value       = { for k, v in azurerm_policy_definition.policy_definitions : k => v.display_name }
 }
 output "policy_definitions_management_group_id" {
-  description = "List of management_group_id values across all policy_definitions"
-  value       = [for k, v in azurerm_policy_definition.policy_definitions : v.management_group_id]
+  description = "Map of management_group_id values across all policy_definitions, keyed the same as var.policy_definitions"
+  value       = { for k, v in azurerm_policy_definition.policy_definitions : k => v.management_group_id }
 }
 output "policy_definitions_metadata" {
-  description = "List of metadata values across all policy_definitions"
-  value       = [for k, v in azurerm_policy_definition.policy_definitions : v.metadata]
+  description = "Map of metadata values across all policy_definitions, keyed the same as var.policy_definitions"
+  value       = { for k, v in azurerm_policy_definition.policy_definitions : k => v.metadata }
 }
 output "policy_definitions_mode" {
-  description = "List of mode values across all policy_definitions"
-  value       = [for k, v in azurerm_policy_definition.policy_definitions : v.mode]
+  description = "Map of mode values across all policy_definitions, keyed the same as var.policy_definitions"
+  value       = { for k, v in azurerm_policy_definition.policy_definitions : k => v.mode }
 }
 output "policy_definitions_name" {
-  description = "List of name values across all policy_definitions"
-  value       = [for k, v in azurerm_policy_definition.policy_definitions : v.name]
+  description = "Map of name values across all policy_definitions, keyed the same as var.policy_definitions"
+  value       = { for k, v in azurerm_policy_definition.policy_definitions : k => v.name }
 }
 output "policy_definitions_parameters" {
-  description = "List of parameters values across all policy_definitions"
-  value       = [for k, v in azurerm_policy_definition.policy_definitions : v.parameters]
+  description = "Map of parameters values across all policy_definitions, keyed the same as var.policy_definitions"
+  value       = { for k, v in azurerm_policy_definition.policy_definitions : k => v.parameters }
 }
 output "policy_definitions_policy_rule" {
-  description = "List of policy_rule values across all policy_definitions"
-  value       = [for k, v in azurerm_policy_definition.policy_definitions : v.policy_rule]
+  description = "Map of policy_rule values across all policy_definitions, keyed the same as var.policy_definitions"
+  value       = { for k, v in azurerm_policy_definition.policy_definitions : k => v.policy_rule }
 }
 output "policy_definitions_policy_type" {
-  description = "List of policy_type values across all policy_definitions"
-  value       = [for k, v in azurerm_policy_definition.policy_definitions : v.policy_type]
+  description = "Map of policy_type values across all policy_definitions, keyed the same as var.policy_definitions"
+  value       = { for k, v in azurerm_policy_definition.policy_definitions : k => v.policy_type }
 }
 output "policy_definitions_role_definition_ids" {
-  description = "List of role_definition_ids values across all policy_definitions"
-  value       = [for k, v in azurerm_policy_definition.policy_definitions : v.role_definition_ids]
+  description = "Map of role_definition_ids values across all policy_definitions, keyed the same as var.policy_definitions"
+  value       = { for k, v in azurerm_policy_definition.policy_definitions : k => v.role_definition_ids }
 }
 
